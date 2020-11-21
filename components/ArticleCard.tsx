049f358d67da1a7605dead06b9457766db5ba3e1
krittinuns <krittinun.sirodom@gmail.com>
2020-11-21 23:51:49 +0700

    [home] add fade animation

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -1,10 +1,12 @@
 import { useRouter } from 'next/router'
+
 import { makeStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import CardActionArea from '@material-ui/core/CardActionArea'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
+import Fade from '@material-ui/core/Fade'
 
 const useStyles = makeStyles({
   root: {
@@ -32,32 +34,34 @@ const ArticleCard = (info: CardInfo): JSX.Element => {
   const router = useRouter()
 
   return (
-    <CardActionArea
-      onClick={(e) => {
-        e.preventDefault()
-        const slug = info.web_url
-          .replace(`${process.env.NEXT_PUBLIC_NYT_WEB_URL}/`, '')
-          .replace('.html', '')
-        router.push(`/post/${slug}`).then(() => window.scrollTo(0, 0))
-      }}
-    >
-      <Card className={classes.root}>
-        <CardMedia className={classes.media} image={info.imageUrl} title={info.title} />
-        <CardContent>
-          <Typography variant="overline">{info.category}</Typography>
-          <Typography variant="h6">{info.title}</Typography>
-          <Typography variant="caption" color="textSecondary">
-            {info.author}
-          </Typography>
-          <Typography gutterBottom variant="caption" color="textSecondary">
-            {new Date(info.date).toISOString().slice(0, 10)}
-          </Typography>
-          <Typography variant="body2" color="textSecondary">
-            {info.body}
-          </Typography>
-        </CardContent>
-      </Card>{' '}
-    </CardActionArea>
+    <Fade in timeout={500}>
+      <CardActionArea
+        onClick={(e) => {
+          e.preventDefault()
+          const slug = info.web_url
+            .replace(`${process.env.NEXT_PUBLIC_NYT_WEB_URL}/`, '')
+            .replace('.html', '')
+          router.push(`/post/${slug}`).then(() => window.scrollTo(0, 0))
+        }}
+      >
+        <Card className={classes.root}>
+          <CardMedia className={classes.media} image={info.imageUrl} title={info.title} />
+          <CardContent>
+            <Typography variant="overline">{info.category}</Typography>
+            <Typography variant="h6">{info.title}</Typography>
+            <Typography variant="caption" color="textSecondary">
+              {info.author}
+            </Typography>
+            <Typography gutterBottom variant="caption" color="textSecondary">
+              {new Date(info.date).toISOString().slice(0, 10)}
+            </Typography>
+            <Typography variant="body2" color="textSecondary">
+              {info.body}
+            </Typography>
+          </CardContent>
+        </Card>{' '}
+      </CardActionArea>
+    </Fade>
   )
 }
 
